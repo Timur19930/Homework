8b26a103cc7a6cdaeb445715b1489bae349a1a42
Timur <golubev_t@mail.ru>
2022-12-19 21:01:38 +0300

    commit1

---
+++ Seminar4/task1/task1.cs
@@ -0,0 +1,17 @@
+﻿int numberA = ReadInt("Введите число А: ");
+int numberB = ReadInt("Введите число В: ");
+ToDeegree(numberA, numberB);
+void ToDeegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+int ReadLine(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
