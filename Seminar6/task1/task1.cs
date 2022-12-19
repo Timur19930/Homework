069cbb83efacbf930969eb15db1e932699dd299e
Timur <golubev_t@mail.ru>
2022-12-19 22:54:55 +0300

    commit1

---
+++ Seminar6/task1/task1.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите числа: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Кол-во чисел > 0: {count}");
